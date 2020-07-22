e783c796532137bb8bdf1263e101166d19aee631
patison5 <patison4@yandex.ru>
2020-07-22 22:10:22 +0300

    пуш

--- js/app.js
+++ js/app.js
@@ -4,8 +4,27 @@
 // "I_7"  - терминал номер 2  (метка справа сбоку)
 
 var terminalNumber = "I_20"; // Установлен терминал номер 1
+var timeBeforeStop =  30;   // время возвращения в исходное состояние - дефолт 30 секунд
+
+// эти переменные не трогать
+var selectedMark = ""       // переменная для сохранения выбранной метки
+var selectedMarkInterval;   // переменная анимации пульсации для иконки нахождения человека
 
 window.onload = function () {
+
+    if (terminalNumber == "I_20") {
+        document.getElementById("I_6").style.display = "block";
+        selectedMark = "I_6";
+    }
+    else {
+        document.getElementById("I_55").style.display = "block";
+        selectedMark = "I_55";
+    }
+
+    // интервал запускающий анимацию пульсации метки нахождения человека
+    selectedMarkInterval = setInterval(function () {
+        animate([selectedMark])
+    }, 1800);
 	
 	// var object = document.getElementById('svg_plan-js');
 	var svgDocument = document.getElementById('svg_plan-js');
@@ -13,9 +32,17 @@ window.onload = function () {
  	
     var btn = document.getElementsByClassName('menu__item')
     var animeTimer;
+    var animeTimeout;
+    var prevSelected;
 
     for (var i = 0; i < btn.length; i++) {
     	btn[i].addEventListener('click', function () {
+
+    		if (prevSelected)
+    			prevSelected.style.border = '1px solid transparent';
+
+    		prevSelected = this.getElementsByClassName('meun-item__text')[0];
+       		prevSelected.style.border = '1px solid red';
     		
     		var datasetValue = this.dataset.value;
     		var iconLink = this.dataset.icon;
@@ -25,12 +52,19 @@ window.onload = function () {
     			rightAssede.style.backgroundSize = "85% auto";
 
     		clearInterval(animeTimer);
+    		clearTimeout(animeTimeout);
 
     		document.getElementById('O_Zone').style.opacity = "0";
     		document.getElementById('K_Zone').style.opacity = "0";
 
     		if ((datasetValue == 'ico-O_Zone') || (datasetValue == 'ico-K_Zone')) {
     			document.getElementById(datasetValue.replace('ico-', '')).style.opacity = "1";
+    			animeTimeout = setTimeout(function () {
+	    			document.getElementById('O_Zone').style.opacity = "0";
+    				document.getElementById('K_Zone').style.opacity = "0";
+    				clearBorder();
+	    		}, timeBeforeStop * 1000)
+
     		} else {
     			var arr = getAppropriateArray(datasetValue);
 
@@ -38,10 +72,25 @@ window.onload = function () {
 	    		animeTimer = setInterval(function () {
 					animate(arr)
 	    		}, 1800);
+
+	    		animeTimeout = setTimeout(function () {
+	    			clearInterval(animeTimer)
+	    			clearBorder();
+
+
+
+                    // интервал возобновляющий мигание стационарной кнопки нахождения человека
+                    selectedMarkInterval = setInterval(function () {
+                        animate([selectedMark])
+                    }, 1800);
+	    		}, timeBeforeStop * 1000)
     		}
+
+
+            // при клике на элемент меню останавливаем мигание активного элемнта
+            clearInterval(selectedMarkInterval);
 	    })
     }
-
 
     // Реализация пульсации иконки
     function animate (arr) {
@@ -51,16 +100,27 @@ window.onload = function () {
 		}, 800)
 	}
 
+	function clearBorder () {
+		var menuElements = document.getElementsByClassName('meun-item__text');
+		console.log("stoping animation")
+
+
+		for (var i = 0; i < menuElements.length; i++) {
+			var element = menuElements[i];
+			element.style.border = '1px solid transparent';
+		}
+	}
+
 	// Запускаю анимацию для иконки терминала	
-	setInterval(function () {
-		animate([terminalNumber])
-	}, 1800);
+	// setInterval(function () {
+	// 	animate([terminalNumber])
+	// }, 1800);
 
 	// Реализация изменения размера иконки относительно ее позиции, а также замена цвета обводки 
     function resizeMarks (elements, diselect) {
     	for (var i = 0; i < elements.length; i++) {
     		var elementId = elements[i];
-		    var scalingValue = (diselect) ? 1 : (elementId.includes("R_")) ? 1.3 : 2;
+		    var scalingValue = (diselect) ? 1 : (elementId.includes("R_")) ? 1.3 : 3;
 
 		    var smallGroup = document.getElementById(elementId);
 		    // console.log(elementId)
@@ -98,11 +158,13 @@ window.onload = function () {
 
     	switch (title) {
     		case "ico-peregovory_01_11":
-    			for (var i = 1; i <= 11; i++)
-    				if (i <= 9)
-    					thisArray.push("B_0" + i)
-    				else
-    					thisArray.push("B_" + i)
+    			for (var i = 1; i <= 10; i++)
+    				// if (i <= 9)
+    				// 	thisArray.push("B_0" + i)
+    				// else
+    				// 	thisArray.push("B_" + i)
+
+                    thisArray.push("B_01", "B_02", "B_03", "B_04", "B_05", "B_06", "B_07", "B_08", "B_09", "B_11")
     			break;
 
     		case "ico-peregovory_16_36":
@@ -115,7 +177,7 @@ window.onload = function () {
     			break;
 
     		case "ico-ochered":
-    			thisArray.push("I_21", "I_23", "I_22")
+    			thisArray.push("I_21", "I_23", "I_22", "I_51")
     			break;
 
     		case "ico-kassy":
@@ -151,7 +213,7 @@ window.onload = function () {
     			break;
 
     		case "ico-toilet":
-    			thisArray.push("I_19", "I_17")
+    			thisArray.push("I_19", "I_17", "I_16", "I_15", "I_18")
     			break;
 
     		case "ico-window_1_4":
@@ -159,7 +221,7 @@ window.onload = function () {
     			break;
 
 			case "ico-window_5_7":
-    			thisArray.push("R_5", "R_6", "R_7")
+    			thisArray.push("R_5", "R_6", "R_7", "I_3")
     			break;
 
     		case "ico-window_8_12":
@@ -167,7 +229,8 @@ window.onload = function () {
     			break;
 
     		case "ico-window_13_29":
-    			thisArray.push("R_13", "R_14", "R_15", "R_16", "R_17", "R_18", "R_19", "R_20", "R_21", "R_22", "R_28", "R_29")
+    			thisArray.push("R_13", "R_14", "R_15", "R_16", "R_17", "R_18", "R_19", "R_20", "R_21", "R_22", 
+    						   "R_28", "R_29", "I_12", "I_8", "I_9", "I_10", "I_14", "I_2", "I_4", "I_5")
     			break;
 
     		case "ico-notarius":
@@ -190,6 +253,13 @@ window.onload = function () {
     	return thisArray;
     }
 }
+
+
+window.addEventListener('touchstart', function(e) {
+  if (e.targetTouches.length === 2) {
+    e.preventDefault();
+  }
+}, false);
 
 
 // нотариус
